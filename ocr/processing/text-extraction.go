82ee5da567df78661203f6a31db5457ccda88e18
Lezh1k <lezh1k.vohrer@gmail.com>
2018-02-05 18:42:30 +0600

    First version without fill percent, but with all needed transformations

--- ocr/processing/text-extraction.go
+++ ocr/processing/text-extraction.go
@@ -3,12 +3,14 @@ package processing
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"fmt"
 	"image"
 	"image/color"
 	"os"
 	"strconv"
 
 	"github.com/otiai10/gosseract"
+	"github.com/tzununbekov/go-idmatch/utils"
 	"gocv.io/x/gocv"
 )
 
@@ -18,41 +20,100 @@ type block struct {
 }
 
 func TextRegions(img gocv.Mat) [][]image.Point {
-	binarized := gocv.NewMat()
-	gocv.CvtColor(img, binarized, gocv.ColorBGRToGray)
-	kernel := gocv.GetStructuringElement(gocv.MorphEllipse, image.Point{5, 5})
-	gocv.MorphologyEx(binarized, binarized, gocv.MorphGradient, kernel)
-	gocv.Threshold(binarized, binarized, 0, 255, gocv.ThresholdBinary|gocv.ThresholdOtsu)
+	/*old golang variant*/
+	// binarized := gocv.NewMat()
+	// gocv.CvtColor(img, binarized, gocv.ColorBGRToGray)
+	// kernel := gocv.GetStructuringElement(gocv.MorphEllipse, image.Point{5, 5})
+	// gocv.MorphologyEx(binarized, binarized, gocv.MorphGradient, kernel)
+	// gocv.Threshold(binarized, binarized, 0, 255, gocv.ThresholdBinary|gocv.ThresholdOtsu)
 
+	// connected := gocv.NewMat()
+	// kernel = gocv.GetStructuringElement(gocv.MorphEllipse, image.Point{9, 1})
+	// gocv.MorphologyEx(binarized, connected, gocv.MorphClose, kernel)
+
+	// return gocv.FindContours(connected, gocv.RetrievalCComp, gocv.ChainApproxSimple)
+	/*old python variant*/
+	original := img.Clone()
+
+	gray := gocv.NewMat()
+	defer gray.Close()
+
+	kernel := gocv.GetStructuringElement(gocv.MorphEllipse, image.Point{5, 7})
+	defer kernel.Close()
+
+	opening := gocv.NewMat()
+	defer opening.Close()
+	gocv.MorphologyEx(original, opening, gocv.MorphGradient, kernel)
+
+	gocv.CvtColor(opening, gray, gocv.ColorBGRToGray)
+	binarization := gocv.NewMat()
+	defer binarization.Close()
+	gocv.Threshold(gray, binarization, 0.0, 255.0, gocv.ThresholdBinary|gocv.ThresholdOtsu)
+
+	kernel = gocv.GetStructuringElement(gocv.MorphRect, image.Point{9, 1})
 	connected := gocv.NewMat()
-	kernel = gocv.GetStructuringElement(gocv.MorphEllipse, image.Point{9, 1})
-	gocv.MorphologyEx(binarized, connected, gocv.MorphClose, kernel)
+	defer connected.Close()
 
+	gocv.MorphologyEx(binarization, connected, gocv.MorphClose, kernel)
 	return gocv.FindContours(connected, gocv.RetrievalCComp, gocv.ChainApproxSimple)
-}
 
-func extractText(file string) (string, error) {
-	client := gosseract.NewClient()
-	defer client.Close()
-	client.SetLanguage("kir", "eng")
-	client.SetImage(file)
-	return client.Text()
+	/*old C variant*/
+	// gray := gocv.NewMat()
+	// defer gray.Close()
+
+	// grad := gocv.NewMat()
+	// defer grad.Close()
+
+	// binarized := gocv.NewMat()
+	// defer binarized.Close()
+
+	// kernel := gocv.GetStructuringElement(gocv.MorphEllipse, image.Point{5, 7})
+	// defer kernel.Close()
+
+	// //convert to gray
+	// gocv.CvtColor(img, gray, gocv.ColorBGRToGray)
+	// gocv.MorphologyEx(gray, grad, gocv.MorphGradient, kernel)
+	// gocv.Threshold(grad, binarized, 0, 255, gocv.ThresholdBinary|gocv.ThresholdOtsu)
+
+	// connected := gocv.NewMat()
+	// defer connected.Close()
+
+	// kernel = gocv.GetStructuringElement(gocv.MorphRect, image.Point{9, 1})
+	// gocv.MorphologyEx(binarized, connected, gocv.MorphClose, kernel)
+
+	// return gocv.FindContours(connected, gocv.RetrievalCComp, gocv.ChainApproxSimple)
 }
 
 func RecognizeRegions(img gocv.Mat, regions [][]image.Point, preview string) (result []block, path string) {
+
+	fmt.Println(preview)
+	client := gosseract.NewClient()
+	defer client.Close()
+	client.SetLanguage("rus", "eng")
+
+	gray := gocv.NewMat()
+	defer gray.Close()
+
+	gocv.CvtColor(img, gray, gocv.ColorBGRToGray)
+
 	for k, v := range regions {
 		region := gocv.BoundingRect(v)
 		// Replace absolute size with relative values
-		if region.Dx() < 16 || region.Dy() < 16 || region.Dy() > 64 {
+		if region.Dx() < 20 || region.Dy() < 20 {
 			continue
 		}
-		roi := img.Region(region)
+
+		roi := gray.Region(region)
 		file := strconv.Itoa(k) + ".jpeg"
+
 		gocv.IMWrite(file, roi)
-		text, err := extractText(file)
+		client.SetImage(file)
+		text, err := client.Text()
+
 		if err != nil {
 			continue
 		}
+
 		result = append(result, block{
 			x:    float64(region.Min.X) / float64(img.Cols()),
 			y:    float64(region.Min.Y) / float64(img.Rows()),
@@ -60,15 +121,17 @@ func RecognizeRegions(img gocv.Mat, regions [][]image.Point, preview string) (re
 			h:    float64(region.Dy()) / float64(img.Rows()),
 			text: text,
 		})
+
 		os.Remove(file)
 		gocv.Rectangle(img, gocv.BoundingRect(v), color.RGBA{255, 0, 0, 255}, 2)
 	}
+
 	if len(preview) != 0 {
 		hash := md5.New()
 		hash.Write(img.ToBytes())
 		path = preview + "/" + hex.EncodeToString(hash.Sum(nil)) + ".jpeg"
 		gocv.IMWrite(path, img)
-		// utils.ShowImage(img)
+		utils.ShowImage(img)
 	}
 	return result, path
 }
