c8889f8a963c39eb51af6f6523e15ad01563977a
tzununbekov <t.zununbekov@gmail.com>
2018-02-20 12:57:02 +0600

    Genetic algorithm improvements

--- ocr/preprocessing/contours.go
+++ ocr/preprocessing/contours.go
@@ -1,6 +1,7 @@
 package preprocessing
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"math"
@@ -51,45 +52,25 @@ func Contours(file string, card templates.Card) gocv.Mat {
 	defer cleanCanny.Close()
 
 	img := gocv.IMRead(file, gocv.IMReadColor)
+	k := float64(560) / float64(img.Rows())
+	gocv.Resize(img, img, image.Point{0, 0}, k, k, gocv.InterpolationCubic)
 	img.CopyTo(original)
 	gocv.ApplyColorMap(img, img, gocv.ColormapHot)
 	gocv.CvtColor(img, img, gocv.ColorRGBToGray)
 	Img = gocv.NewMat()
 	img.CopyTo(Img)
-	gocv.GaussianBlur(img, cleanCanny, image.Point{config.Preprocessing.CleanCannyBlurSize, config.Preprocessing.CleanCannyBlurSize},
-		config.Preprocessing.CleanCannyBlurSigma, config.Preprocessing.CleanCannyBlurSigma, gocv.BorderDefault)
-	gocv.Canny(cleanCanny, cleanCanny, config.Preprocessing.CleanCannyT1, config.Preprocessing.CleanCannyT2)
-
-	rotation := rotate(cleanCanny)
-	gocv.WarpAffine(img, img, rotation, image.Point{img.Cols(), img.Rows()})
-	gocv.WarpAffine(original, original, rotation, image.Point{original.Cols(), original.Rows()})
 
 	OriginalArea = original.Cols() * original.Rows()
-	OriginalRatio = float64(85.60) / float64(53.98)
+	OriginalRatio = card.AspectRatio
 
-	val := GetFactors()
+	GetFactors()
 	defer Img.Close()
 
-	if int(val[0])%2 != 1 {
-		val[0]++
-	}
-
-	gocv.MedianBlur(img, img, int(val[0]))
-	gocv.Canny(img, img, float32(val[1]), float32(val[2]))
-	contours := gocv.FindContours(img, gocv.RetrievalExternal, gocv.ChainApproxSimple)
-	var maxArea int
-	var maxRect image.Rectangle
-	for _, v := range contours {
-		rect := gocv.BoundingRect(v)
-		if area := rect.Dx() * rect.Dy(); area > maxArea {
-			maxArea = area
-			maxRect = rect
-		}
-	}
 	if log.IsDebug() {
-		gocv.Rectangle(original, maxRect, color.RGBA{255, 0, 0, 255}, 2)
-		utils.ShowImage(img)
-		utils.ShowImage(original.Region(maxRect))
+		gocv.Rectangle(original, Rect, color.RGBA{255, 0, 0, 255}, 2)
+		// utils.ShowImage(img)
+		fmt.Println(Rect)
+		utils.ShowImage(original.Region(Rect))
 	}
-	return original.Region(maxRect)
+	return original.Region(Rect)
 }
--- ocr/preprocessing/ga.go
+++ ocr/preprocessing/ga.go
@@ -2,8 +2,9 @@ package preprocessing
 
 import (
 	"fmt"
+	"image"
+	"math"
 	"math/rand"
-	"reflect"
 
 	"github.com/MaxHalford/gago"
 	"github.com/maddevsio/go-idmatch/log"
@@ -16,89 +17,84 @@ type Factors struct {
 	CannyT2 []float64
 }
 
-func (X Factors) Evaluate() (float64, error) {
-	// var ratio float64
+var Rect image.Rectangle
 
+func (X Factors) Evaluate() (float64, error) {
+	var res float64
 	child := gocv.NewMat()
 	defer child.Close()
 
-	a, b := float32(X.CannyT1[0]), float32(X.CannyT2[0])
+	a, b := float32(math.Abs(X.CannyT1[0])), float32(math.Abs(X.CannyT2[0]))
 	c := int(X.Blur[0])
 	if c%2 != 1 {
 		c++
-	} else if c <= 0 {
-		c = 1
 	}
-
-	X.Blur = []float64{float64(int(c))}
-
+	if c <= 0 {
+		c = 1
+	} else if c > 21 {
+		c = 21
+	}
 	log.Print(log.DebugLevel, fmt.Sprintf("Blur: %v, canny min: %v, canny max: %v\n", c, a, b))
 
 	gocv.MedianBlur(Img, child, c)
 	gocv.Canny(child, child, a, b)
 
+	var maxArea int
+	var maxRect image.Rectangle
 	contours := gocv.FindContours(child, gocv.RetrievalTree, gocv.ChainApproxSimple)
-	area := 100000
-	if len(contours) > 0 {
-		rect := gocv.BoundingRect(contours[0])
-		area = rect.Dx() * rect.Dy()
+	for _, v := range contours {
+		rect := gocv.BoundingRect(v)
+		if area := rect.Dx() * rect.Dy(); area > maxArea {
+			maxArea = area
+			maxRect = rect
+		}
 	}
-	// maxArea = area
-	// ratio = float64(rect.Dx()) / float64(rect.Dy())
-	// fmt.Println(OriginalRatio-ratio, 1-float64(area)/float64(OriginalArea))
-	// gocv.Rectangle(img3, rect, color.RGBA{255, 0, 0, 255}, 2)
-	return float64(OriginalArea - area), nil
+
+	ratio := float64(maxRect.Dx()) / float64(maxRect.Dy())
+	deltaArea := OriginalArea - maxArea
+	deltaRatio := math.Abs(OriginalRatio - ratio)
+	res = float64(deltaArea)
+	if deltaRatio < 0.1 {
+		res *= deltaRatio
+	}
+
+	log.Print(log.DebugLevel, fmt.Sprintf("rect: %v, dRatio: %v, dArea: %v, result: %v\n", maxRect, deltaRatio, deltaArea, res))
+	Rect = maxRect
+
+	return float64(res), nil
 }
 
-func (X Factors) Mutate(rng *rand.Rand) {
-	// gago.MutNormalFloat64(X.Blur, 0.1, rng)
-	// gago.MutNormalFloat64(X.CannyT1, 0.01, rng)
-	// gago.MutNormalFloat64(X.CannyT2, 0.01, rng)
-}
+func (X Factors) Mutate(rng *rand.Rand) {}
 
-func (X Factors) Crossover(Y gago.Genome, rng *rand.Rand) {
-	gago.CrossUniformFloat64(X.Blur, Y.(Factors).Blur, rng)
-	gago.CrossUniformFloat64(X.CannyT1, Y.(Factors).CannyT1, rng)
-	gago.CrossUniformFloat64(X.CannyT2, Y.(Factors).CannyT2, rng)
-}
+func (X Factors) Crossover(Y gago.Genome, rng *rand.Rand) {}
 
 func (X Factors) Clone() gago.Genome {
-	Y := Factors{
+	return Factors{
 		Blur:    X.Blur,
 		CannyT1: X.CannyT1,
 		CannyT2: X.CannyT2,
 	}
-	return Y
 }
 
 func InitValues(rng *rand.Rand) gago.Genome {
-	var X Factors
-	X.Blur = gago.InitUnifFloat64(1, 1, 17, rng)
-	X.CannyT1 = gago.InitUnifFloat64(1, 1, 50, rng)
-	X.CannyT2 = gago.InitUnifFloat64(1, 50, 200, rng)
-	return X
+	return Factors{
+		Blur:    gago.InitUnifFloat64(1, 1, 21, rng),
+		CannyT1: gago.InitUnifFloat64(1, 1, 30, rng),
+		CannyT2: gago.InitUnifFloat64(1, 30, 180, rng),
+	}
 }
 
-func GetFactors() (res []float64) {
+func GetFactors() {
 	var ga = gago.Generational(InitValues)
+	ga.ParallelEval = true
 	if err := ga.Initialize(); err != nil {
 		log.Print(log.WarnLevel, err.Error())
 	}
 
-	for i := 1; i < 7; i++ {
+	for i := 1; i < 4; i++ {
 		if err := ga.Evolve(); err != nil {
 			log.Print(log.WarnLevel, err.Error())
 		}
 		fmt.Printf("Best fitness at generation %d: %f (%v)\n", i, ga.HallOfFame[0].Fitness, ga.HallOfFame[0].Genome)
 	}
-
-	t := reflect.ValueOf(ga.HallOfFame[0].Genome)
-	for i := 0; i < t.NumField(); i++ {
-		slice, ok := t.Field(i).Interface().([]float64)
-		if !ok {
-			log.Print(log.ErrorLevel, "Error decoding genome value")
-		}
-		res = append(res, slice[0])
-	}
-	return res
 }
